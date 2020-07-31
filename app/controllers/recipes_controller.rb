7e53fa802ac93077ef4de593e90819fabfa30abf
Joan Racenet <joan.racenet@hiptest.net>
2020-07-31 14:29:51 +0200

    Add a basic search (using only one ingredient at a time)

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,8 @@
 class RecipesController < ApplicationController
   def search_by_ingredients
-    render json: { recipes: Recipe.all }, status: :ok
+    query = params[:query]
+    matches = Recipe.joins(:ingredients).where('ingredients.raw_marmiton_entry LIKE :query', query: "%#{query}%")
+
+    render json: { recipes: matches }, status: :ok
   end
 end
--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -4,19 +4,86 @@ RSpec.describe RecipesController, type: :controller do
   describe "GET search_by_ingredients" do
     subject { get :search_by_ingredients }
 
-    let(:recipes) { create_list(:recipe, 25) }
-
-    before do
-      recipes
-    end
-
     it { is_expected.to have_http_status(:success)}
 
-    it 'returns all recipes' do
-      subject
+    context 'one ingredient' do
+      subject { get :search_by_ingredients, params: { query: 'egg' } }
 
-      recipes_response = JSON.parse(response.body)
-      expect(recipes_response['recipes']).to eq recipes.as_json
+      let(:cake_recipe) {
+        recipe = create(:recipe, name: 'Cake dough')
+        recipe.ingredients.create!([
+          {
+            raw_marmiton_entry: '150gr of flour'
+          },
+          {
+            raw_marmiton_entry: '3 eggs'
+          },
+          {
+            raw_marmiton_entry: '150gr of flour'
+          }
+        ])
+
+        recipe
+      }
+
+      let(:salade_recipe) {
+        recipe = create(:recipe, name: 'Caesar salad')
+        recipe.ingredients.create!([
+          {
+            raw_marmiton_entry: '1 romana salad'
+          },
+          {
+            raw_marmiton_entry: '3 chicken breasts'
+          },
+          {
+            raw_marmiton_entry: '1 egg'
+          },
+          {
+            raw_marmiton_entry: 'Oil'
+          },
+          {
+            raw_marmiton_entry: 'Dijon mustard'
+          }
+        ])
+
+        recipe
+      }
+
+      let(:ratatouille_recipe) {
+        recipe = create(:recipe, name: 'Ratatouille')
+        recipe.ingredients.create!([
+          {
+            raw_marmiton_entry: '3 red peppers'
+          },
+          {
+            raw_marmiton_entry: '3 courgettes'
+          },
+          {
+            raw_marmiton_entry: '1 onion'
+          },
+          {
+            raw_marmiton_entry: 'Oil'
+          },
+        ])
+
+        recipe
+
+      }
+
+      let(:recipes) {
+        [ cake_recipe, salade_recipe, ratatouille_recipe ]
+      }
+
+      before do
+        recipes
+      end
+
+      it 'returns recipes that use eggs' do
+        subject
+        recipes_response = JSON.parse(response.body)
+
+        expect(recipes_response['recipes']).to eq [cake_recipe, salade_recipe].as_json
+      end
     end
   end
 end
