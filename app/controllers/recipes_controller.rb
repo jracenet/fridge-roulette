3ed60854cf581a1a86408522562455dbe3cc6427
Joan Racenet <joan.racenet@hiptest.net>
2020-08-03 13:42:49 +0200

    Fix search with accent

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ require "i18n"
 
 class RecipesController < ApplicationController
   def search_by_ingredients
-    query = I18n.transliterate(params[:query])
+    query = params[:query]
     matches = Recipe.search_by_ingredient(query).uniq
 
     render json: { recipes: matches }, status: :ok
